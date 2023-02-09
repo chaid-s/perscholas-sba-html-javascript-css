9ac6fc9b765f208848bceed096870d094f502b23
chaid-s <31129188+chaid-s@users.noreply.github.com>
2023-02-09 14:15:33 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function getOptions() {
+  var x = document.getElementById("mySelect");
+  var txt = "";
+  var i;
+  for (i = 0; i < x.length; i++) {
+    txt = txt + " " + x.options[i].text;
+  }
+  document.getElementById("outputresult").innerHTML = txt;
+}
